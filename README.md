8cddfad65fb008a06b0779f330a23d433fa92a69
Sokngoun Chan <sokngoun.chan@gmail.com>
2023-04-25 09:58:46 +0700

    update

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-API_URL ="https://api.themoviedb.org/3/movie/popular?api_key=71cfbd1cf49dc81c483c34e89e6397b1"
+API_URL ="https://api.themoviedb.org/3/movie/popular?api_key="
 API_IMG ='https://image.tmdb.org/t/p/w500/'
-API_SEARCH='https://api.themoviedb.org/3/search/movie?api_key=71cfbd1cf49dc81c483c34e89e6397b1&query'
+API_SEARCH='https://api.themoviedb.org/3/search/movie?api_key=&query'
--- src/App.js
+++ src/App.js
@@ -1,82 +1,91 @@
-import React,{useEffect, useState} from 'react';
-import './App.css';
-import { MovieBox } from './components/MovieBox';
-import 'bootstrap/dist/css/bootstrap.css';
-import {Navbar,Container,Nav,Form, FormControl,Button} from 'react-bootstrap'
-const API_URL ="https://api.themoviedb.org/3/movie/popular?api_key=71cfbd1cf49dc81c483c34e89e6397b1";
-const API_SEARCH='https://api.themoviedb.org/3/search/movie?api_key=71cfbd1cf49dc81c483c34e89e6397b1&query'
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import { MovieBox } from "./components/MovieBox";
+import "bootstrap/dist/css/bootstrap.css";
+import {
+  Navbar,
+  Container,
+  Nav,
+  Form,
+  FormControl,
+  Button,
+} from "react-bootstrap";
+const API_URL = `https://api.themoviedb.org/3/movie/popular?api_key=${process.env.REACT_APP_API_KEY}`;
+const API_SEARCH = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.REACT_APP_API_KEY}&query`;
 function App() {
-  const [movies,setMovies] = useState([]);
-  const [examples,setExamples] = useState(['dra','hong']);
-  const [query,setQuery] = useState('');
-  useEffect(()=>{
+  const [movies, setMovies] = useState([]);
+  const [examples, setExamples] = useState(["dra", "hong"]);
+  const [query, setQuery] = useState("");
+  useEffect(() => {
     fetch(API_URL)
-    .then((res)=>res.json())
-    .then(data=>{
-      console.log(data)
-      setMovies(data.results)
-    })
-  },[])
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setMovies(data.results);
+      });
+  }, []);
 
-const searchMovie = async(e)=>{
-  e.preventDefault()
-  console.log("searching");
-  try{
-    const url=`https://api.themoviedb.org/3/search/movie?api_key=71cfbd1cf49dc81c483c34e89e6397b1&query=${query}`
-    const res= await fetch(url);
-    const data = await res.json();
-    console.log(data)
-    setMovies(data.results)
-  }catch(e){
-    console.log(e);
-  }
-}
-  const changeHandler=(e)=>{
+  const searchMovie = async (e) => {
+    e.preventDefault();
+    console.log("searching");
+    try {
+      const url = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.REACT_APP_API_KEY}&query=${query}`;
+      const res = await fetch(url);
+      const data = await res.json();
+      console.log(data);
+      setMovies(data.results);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+  const changeHandler = (e) => {
     setQuery(e.target.value);
-  }
+  };
 
   return (
-   <>
-    <Navbar bg="dark" expand="lg" variant="dark">
-      <Container fluid>
-        <Navbar.Brand href="/home">MovieDB App</Navbar.Brand>
-        <Navbar.Brand href="/home">Trending</Navbar.Brand>
-        <Navbar.Toggle aria-controls="navbavScroll"></Navbar.Toggle>
+    <>
+      <Navbar bg="dark" expand="lg" variant="dark">
+        <Container fluid>
+          <Navbar.Brand href="/home">MovieDB App</Navbar.Brand>
+          <Navbar.Brand href="/home">Trending</Navbar.Brand>
+          <Navbar.Toggle aria-controls="navbavScroll"></Navbar.Toggle>
           <Navbar.Collapse id="navbarScroll">
-            <Nav className="me-auto my-2 my-lg-3"
-            style={{maxHeight:'100px'}}
-            navbarScroll></Nav>
-            <Form className='d-flex' onSubmit={searchMovie}>
+            <Nav
+              className="me-auto my-2 my-lg-3"
+              style={{ maxHeight: "100px" }}
+              navbarScroll
+            ></Nav>
+            <Form className="d-flex" onSubmit={searchMovie}>
               <FormControl
-              type="search"
-              placeholder='Movie Search'
-              className='me-2'
-              aria-label='search'
-              name="query"
-              value={query} onChange={changeHandler}>
-               
-              </FormControl>
-              <Button variant="secondary" type="submit">Search</Button>
+                type="search"
+                placeholder="Movie Search"
+                className="me-2"
+                aria-label="search"
+                name="query"
+                value={query}
+                onChange={changeHandler}
+              ></FormControl>
+              <Button variant="secondary" type="submit">
+                Search
+              </Button>
             </Form>
           </Navbar.Collapse>
-        
-      </Container>
-    </Navbar>
-    <div>
-      {movies.length>0?(
-        <div className="container">
-        <div className='grid'>
-        {
-          movies.map((movieReq)=>
-          <MovieBox key={movieReq.id} {...movieReq}/>)
-        }
-        </div>
+        </Container>
+      </Navbar>
+      <div>
+        {movies.length > 0 ? (
+          <div className="container">
+            <div className="grid">
+              {movies.map((movieReq) => (
+                <MovieBox key={movieReq.id} {...movieReq} />
+              ))}
+            </div>
+          </div>
+        ) : (
+          <h2>Sorry !! No Moview Found</h2>
+        )}
       </div>
-      ):(
-        <h2>Sorry !! No Moview Found</h2>
-      )}
-    </div>
-   </>
+    </>
   );
 }
 
