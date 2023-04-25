e1cbec8ae105dc1c8e8afb066abe4600f0a664e5
Sokngoun Chan <sokngoun.chan@gmail.com>
2023-04-25 10:48:45 +0700

    commit

--- src/App.js
+++ src/App.js
@@ -9,21 +9,24 @@ import {
   Form,
   FormControl,
   Button,
+  Pagination,
 } from "react-bootstrap";
 const API_URL = `https://api.themoviedb.org/3/movie/popular?api_key=${process.env.REACT_APP_API_KEY}`;
 const API_SEARCH = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.REACT_APP_API_KEY}&query`;
 function App() {
   const [movies, setMovies] = useState([]);
+  const [page, setPage] = useState(1);
   const [examples, setExamples] = useState(["dra", "hong"]);
   const [query, setQuery] = useState("");
+
   useEffect(() => {
-    fetch(API_URL)
+    fetch(API_URL + "&page=" + page)
       .then((res) => res.json())
       .then((data) => {
         console.log(data);
         setMovies(data.results);
       });
-  }, []);
+  }, [page]);
 
   const searchMovie = async (e) => {
     e.preventDefault();
@@ -38,9 +41,24 @@ function App() {
       console.log(e);
     }
   };
+
   const changeHandler = (e) => {
     setQuery(e.target.value);
   };
+
+  function generateList(n) {
+    const arr = Array(5);
+    for (let i = 0; i < 5; i++) {
+      if (n < 4) {
+        arr[i] = i + 1;
+      } else {
+        arr[i] = i + n - 2;
+      }
+    }
+    return arr;
+  }
+
+  const list = generateList(page);
 
   return (
     <>
@@ -85,6 +103,24 @@ function App() {
           <h2>Sorry !! No Moview Found</h2>
         )}
       </div>
+
+      <Pagination className="center">
+        <Pagination.Prev
+          onClick={() => setPage(page - 1)}
+          disabled={page <= 1}
+        />
+        {list.map((value) => {
+          return (
+            <Pagination.Item
+              active={value === page}
+              onClick={() => setPage(value)}
+            >
+              {value}
+            </Pagination.Item>
+          );
+        })}
+        <Pagination.Next onClick={() => setPage(page + 1)} />
+      </Pagination>
     </>
   );
 }
